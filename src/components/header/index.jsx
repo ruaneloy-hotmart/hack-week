ae4f5d947c7284f3b4d12714da9786c2c183fdea
Ruan Eloy <ruan.eloy@hotmart.com>
2023-12-05 17:42:25 -0300

    add cosmos web

---
+++ src/components/header/index.jsx
@@ -0,0 +1,173 @@
+import {
+  HcStructure,
+  HcStructureHeader,
+  HcStructureMenu,
+  HcStructureMenuItem,
+  HcStructureContent
+} from "@hotmart-org-ca/cosmos-web/dist/react/hc-structure";
+
+import { HcButton } from "@hotmart-org-ca/cosmos-web/dist/react/hc-button";
+import { HcContainer } from "@hotmart-org-ca/cosmos-web/dist/react/hc-container";
+import { HcH } from "@hotmart-org-ca/cosmos-web/dist/react/hc-h";
+import { HcP } from "@hotmart-org-ca/cosmos-web/dist/react/hc-p";
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBell } from '@fortawesome/pro-light-svg-icons/faBell'
+import { faGrid } from '@fortawesome/pro-light-svg-icons/faGrid'
+import { faMeteor } from '@fortawesome/pro-light-svg-icons/faMeteor'
+import { faPlanetRinged } from '@fortawesome/pro-light-svg-icons/faPlanetRinged'
+import { faRocket } from '@fortawesome/pro-light-svg-icons/faRocket'
+import { faRocketLaunch } from '@fortawesome/pro-light-svg-icons/faRocketLaunch'
+import { faSatellite } from '@fortawesome/pro-light-svg-icons/faSatellite'
+import { faTelescope } from '@fortawesome/pro-light-svg-icons/faTelescope'
+import { faUserAstronaut } from '@fortawesome/pro-light-svg-icons/faUserAstronaut'
+import { faUserCircle } from '@fortawesome/pro-light-svg-icons/faUserCircle'
+
+import './styles.css';
+
+const Header = () => {
+  return (
+    <HcStructure>
+      <HcStructureHeader slot="header">
+        <HcButton slot="actions-end" variation="tertiary" context="neutral" size="large">
+          <div slot="icon">
+            <FontAwesomeIcon icon={faBell} />
+          </div>
+        </HcButton>
+        <HcButton slot="actions-end" variation="tertiary" context="neutral" size="large">
+          <div slot="icon">
+            <FontAwesomeIcon icon={faGrid} />
+          </div>
+        </HcButton>
+        <HcButton slot="actions-end" variation="tertiary" context="neutral" size="large">
+          <div slot="icon">
+            <FontAwesomeIcon icon={faUserCircle} />
+          </div>
+        </HcButton>
+      </HcStructureHeader>
+
+      <HcStructureMenu slot="menu">
+        <HcStructureMenuItem slot="items" onClickItem={() => { console.log('Click') }}>
+          <div slot="icon">
+            <FontAwesomeIcon icon={faRocketLaunch} />
+          </div>
+          Default
+        </HcStructureMenuItem>
+
+        <HcStructureMenuItem slot="items">
+          <div slot="icon">
+            <FontAwesomeIcon icon={faPlanetRinged} />
+          </div>
+          Default
+
+          <HcStructureMenuItem slot="items">
+            <div slot="icon">
+              <FontAwesomeIcon icon={faRocket} />
+            </div>
+            Default
+          </HcStructureMenuItem>
+          <HcStructureMenuItem slot="items">
+            <div slot="icon">
+              <FontAwesomeIcon icon={faRocket} />
+            </div>
+            Default
+          </HcStructureMenuItem>
+          <HcStructureMenuItem slot="items">
+            <div slot="icon">
+              <FontAwesomeIcon icon={faRocket} />
+            </div>
+            Default
+          </HcStructureMenuItem>
+          <HcStructureMenuItem slot="items">
+            <div slot="icon">
+              <FontAwesomeIcon icon={faRocket} />
+            </div>
+            Default
+          </HcStructureMenuItem>
+          <HcStructureMenuItem slot="items">
+            <div slot="icon">
+              <FontAwesomeIcon icon={faRocket} />
+            </div>
+            Default
+          </HcStructureMenuItem>
+          <HcStructureMenuItem slot="items">
+            <div slot="icon">
+              <FontAwesomeIcon icon={faRocket} />
+            </div>
+            Default
+          </HcStructureMenuItem>
+          <HcStructureMenuItem slot="items">
+            <div slot="icon">
+              <FontAwesomeIcon icon={faRocket} />
+            </div>
+            Default
+          </HcStructureMenuItem>
+          <HcStructureMenuItem slot="items">
+            <div slot="icon">
+              <FontAwesomeIcon icon={faRocket} />
+            </div>
+            Default
+
+            <HcStructureMenuItem slot="items">
+              <div slot="icon">
+                <FontAwesomeIcon icon={faRocket} />
+              </div>
+              Default
+            </HcStructureMenuItem>
+            <HcStructureMenuItem slot="items">
+              <div slot="icon">
+                <FontAwesomeIcon icon={faRocket} />
+              </div>
+              Default
+            </HcStructureMenuItem>
+            <HcStructureMenuItem slot="items">
+              <div slot="icon">
+                <FontAwesomeIcon icon={faRocket} />
+              </div>
+              Default
+            </HcStructureMenuItem>
+          </HcStructureMenuItem>
+        </HcStructureMenuItem>
+
+        <HcStructureMenuItem slot="items">
+          <div slot="icon">
+            <FontAwesomeIcon icon={faTelescope} />
+          </div>
+          Default
+        </HcStructureMenuItem>
+
+        <HcStructureMenuItem slot="items">
+          <div slot="icon">
+            <FontAwesomeIcon icon={faMeteor} />
+          </div>
+          Default
+        </HcStructureMenuItem>
+
+        <HcStructureMenuItem slot="items" external-link>
+          <div slot="icon">
+            <FontAwesomeIcon icon={faUserAstronaut} />
+          </div>
+          Default
+        </HcStructureMenuItem>
+
+        <HcStructureMenuItem slot="items">
+          <div slot="icon">
+            <FontAwesomeIcon icon={faSatellite} />
+          </div>
+          Default
+        </HcStructureMenuItem>
+      </HcStructureMenu>
+
+      <HcStructureContent slot="content">
+        {/* <HcContainer> <HcH> <HcP> it's not a part of the structure. Added for demonstration purpose only. */}
+        <HcContainer>
+          <HcH type="h1">My app</HcH>
+          <HcP>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi eaque beatae quam ab tempore, iusto mollitia reiciendis excepturi esse iste ducimus iure eum ut quaerat, perspiciatis nesciunt rerum ea laudantium.</HcP>
+          <HcP>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi eaque beatae quam ab tempore, iusto mollitia reiciendis excepturi esse iste ducimus iure eum ut quaerat, perspiciatis nesciunt rerum ea laudantium.</HcP>
+        </HcContainer>
+      </HcStructureContent>
+    </HcStructure>
+  )
+}
+
+export default Header
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import '@hotmart-org-ca/cosmos-web/dist/web-components/cosmos-web/cosmos-web.css';
+import '@hotmart-org-ca/cosmos-tokens/dist/hotmart-product/base/css/_variables.css';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
